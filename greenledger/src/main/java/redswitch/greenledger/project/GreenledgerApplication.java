184811edc1f7a424cbef9c3471bb28cd6e469611
98rupak <107359788+98rupak@users.noreply.github.com>
2026-03-09 14:14:38 +0530

    Initial commit

---
+++ greenledger/src/main/java/redswitch/greenledger/project/GreenledgerApplication.java
@@ -0,0 +1,13 @@
+package redswitch.greenledger.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenledgerApplication.class, args);
+	}
+
+}
---
+++ greenledger/src/test/java/redswitch/greenledger/project/GreenledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package redswitch.greenledger.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenledgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
